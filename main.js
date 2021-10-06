2da0714a776a56e71f7d12e2350d14ca9f7f1a4f
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-06 17:10:41 +0200

    iteration1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//iteration 1
+
+const btn = document.querySelector('#btnToClick');
+
+function handleClick(e) {
+    console.log(e)
+}
+
+btn.addEventListener('click', handleClick)
+
+//iteration 2
